ac3b87a35b6f4b0a3f286b66c32f4eee7001ba7a
Todd White <twhite@logos.com>
2013-10-10 21:31:05 -0700

    Adding BinaryUtility.

---
+++ src/MCSharp/BinaryUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MCSharp
+{
+    public static class BinaryUtility
+    {
+        public int ConvertBigEndianToInt32(byte[] bytes, int start, int count)
+        {
+	        if (count > 4)
+		        throw new ArgumentOutOfRangeException("count", "Count must be less than 4 bytes.");
+	        if (count <= 0)
+		        throw new ArgumentOutOfRangeException("count", "Count must be greater than 0.");
+
+	        int value = 0;	
+	        for (int byteIndex = 0; byteIndex < count; byteIndex++)
+		        value |= bytes[start + byteIndex] << (count - 1 - byteIndex) * 8;			
+	        return value;
+        }
+    }
+}
