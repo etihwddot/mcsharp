161e7f99d38e3b1859f686b00c232a0b96d64ec5
Jacob Carpenter <jacob.carpenter@logos.com>
2013-10-10 21:25:56 -0700

    Add entirely useless `ChunkLoader` class

---
+++ src/MCSharp/ChunkLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logos.Utility;
+
+namespace MCSharp
+{
+	public sealed class ChunkLoader
+	{
+		public static ChunkLoader Create(string regionsPath)
+		{
+			if (regionsPath == null)
+				throw new ArgumentNullException("regionsPath");
+
+			return new ChunkLoader(regionsPath);
+		}
+
+		private ChunkLoader(string regionsPath)
+		{
+			m_regionsPath = regionsPath;
+		}
+
+		string m_regionsPath;
+	}
+}
