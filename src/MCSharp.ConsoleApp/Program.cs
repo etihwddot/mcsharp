b98e789ab214f2d34e830402b900dba4fc07a41a
Jacob Carpenter <jacob.carpenter@logos.com>
2013-10-12 10:56:17 -0700

    Cleanup
    Also, sorry for committing the save location change... but I don't have
    a Todd folder.

--- src/MCSharp.ConsoleApp/Program.cs
+++ src/MCSharp.ConsoleApp/Program.cs
@@ -10,8 +10,8 @@ namespace MCSharp.ConsoleApp
 	{
 		static void Main(string[] args)
 		{
-			// string regionDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @".minecraft\saves\Mapping\region");
-			string regionDirectory = @"C:\Users\todd\Desktop\region";
+			string regionDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @".minecraft\saves\Mapping\region");
+			//string regionDirectory = @"C:\Users\todd\Desktop\region";
 			string outputLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), @"map.png");
 			
 			const int pixelsPerBlock = 1;
@@ -56,59 +56,44 @@ namespace MCSharp.ConsoleApp
 						for (int z = 0; z < blocksPerChunk; z++)
 						{
 							BiomeKind biome = chunk.GetBiome(x, z);
-
-							if (biome == BiomeKind.Uncalculated)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.Black);
-							else if (biome == BiomeKind.DeepOcean)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.DarkBlue);
-							else if (biome == BiomeKind.Ocean)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.Blue);
-							else if (biome == BiomeKind.River)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.LightBlue);
-							else if (biome == BiomeKind.Beach)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.LightYellow);
-							else if (biome == BiomeKind.SunflowerPlains)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.Green);
-							else if (biome == BiomeKind.Plains)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.Green);
-							else if (biome == BiomeKind.Forest)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.DarkGreen);
-							else if (biome == BiomeKind.ForestHills)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.DarkGreen);
-							else if (biome == BiomeKind.ExtremeHills)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.Brown);
-							else if (biome == BiomeKind.ExtremeHillsEdge)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.SaddleBrown);
-							else if (biome == BiomeKind.Swampland)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.DarkOliveGreen);
-							else if (biome == BiomeKind.Jungle)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.GreenYellow);
-							else if (biome == BiomeKind.JungleHills)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.GreenYellow);
-							else if (biome == BiomeKind.Desert)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.Yellow);
-							else if (biome == BiomeKind.DesertHills)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.Yellow);
-							else if (biome == BiomeKind.Taiga)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.White);
-							else if (biome == BiomeKind.TaigaHills)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.WhiteSmoke);
-							else if (biome == BiomeKind.IcePlains)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.White);
-							else if (biome == BiomeKind.IceMountains)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.White);
-							else if (biome == BiomeKind.FrozenRiver)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.AntiqueWhite);
-							else if (biome == BiomeKind.FrozenOcean)
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.CornflowerBlue);
-							else
-								bitmap.SetPixel(x + xOffset, z + zOffset, Color.Red);
+							Color color = GetColorForBiomeKind(biome);
+							bitmap.SetPixel(x + xOffset, z + zOffset, color);
 						}
 					}
 				}
 			}
 
 			bitmap.Save(outputLocation, ImageFormat.Png);
+		}
+
+		private static Color GetColorForBiomeKind(BiomeKind biome)
+		{
+			switch (biome)
+			{ 
+				case BiomeKind.Uncalculated: return Color.Black;
+				case BiomeKind.DeepOcean: return Color.DarkBlue;
+				case BiomeKind.Ocean: return Color.Blue;
+				case BiomeKind.River: return Color.LightBlue;
+				case BiomeKind.Beach: return Color.LightYellow;
+				case BiomeKind.SunflowerPlains: return Color.Green;
+				case BiomeKind.Plains: return Color.Green;
+				case BiomeKind.Forest: return Color.DarkGreen;
+				case BiomeKind.ForestHills: return Color.DarkGreen;
+				case BiomeKind.ExtremeHills: return Color.Brown;
+				case BiomeKind.ExtremeHillsEdge: return Color.SaddleBrown;
+				case BiomeKind.Swampland: return Color.DarkOliveGreen;
+				case BiomeKind.Jungle: return Color.GreenYellow;
+				case BiomeKind.JungleHills: return Color.GreenYellow;
+				case BiomeKind.Desert: return Color.Yellow;
+				case BiomeKind.DesertHills: return Color.Yellow;
+				case BiomeKind.Taiga: return Color.White;
+				case BiomeKind.TaigaHills: return Color.WhiteSmoke;
+				case BiomeKind.IcePlains: return Color.White;
+				case BiomeKind.IceMountains: return Color.White;
+				case BiomeKind.FrozenRiver: return Color.AntiqueWhite;
+				case BiomeKind.FrozenOcean: return Color.CornflowerBlue;
+				default: return Color.Red;
+			}
 		}
 	}
 }
