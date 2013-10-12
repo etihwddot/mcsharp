c3b0c8f6d180e5b1635ff9ba9ade01eb7287c929
Todd White <twhite@logos.com>
2013-10-11 23:09:06 -0700

    Adding a bunch more ugly colors.

--- src/MCSharp.Console/Program.cs
+++ src/MCSharp.Console/Program.cs
@@ -13,7 +13,8 @@ namespace MCSharp.Console
 	{
 		static void Main(string[] args)
 		{
-			string regionDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @".minecraft\saves\Mapping\region");
+			// string regionDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @".minecraft\saves\Mapping\region");
+			string regionDirectory = @"C:\Users\todd\Desktop\region";
 			string outputLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), @"map.png");
 			
 			const int pixelsPerBlock = 1;
@@ -50,8 +51,8 @@ namespace MCSharp.Console
 				IEnumerable<ChunkData> regionChunks = ChunkLoader.LoadChunksInRegion(regionFile);
 				foreach (ChunkData chunk in regionChunks)
 				{
-					if (!chunk.IsEmpty)
-						System.Console.WriteLine(chunk.Root);
+					//if (!chunk.IsEmpty)
+					//	System.Console.WriteLine(chunk.Root);
 
 					int xOffset = (chunk.XPosition * blocksPerChunk) - (minX * regionSize);
 					int zOffset = (chunk.ZPosition * blocksPerChunk) - (minZ * regionSize);
@@ -78,6 +79,34 @@ namespace MCSharp.Console
 								bitmap.SetPixel(x + xOffset, z + zOffset, Color.Green);
 							else if (biome == BiomeKind.Forest)
 								bitmap.SetPixel(x + xOffset, z + zOffset, Color.DarkGreen);
+							else if (biome == BiomeKind.ForestHills)
+								bitmap.SetPixel(x + xOffset, z + zOffset, Color.DarkGreen);
+							else if (biome == BiomeKind.ExtremeHills)
+								bitmap.SetPixel(x + xOffset, z + zOffset, Color.Brown);
+							else if (biome == BiomeKind.ExtremeHillsEdge)
+								bitmap.SetPixel(x + xOffset, z + zOffset, Color.SaddleBrown);
+							else if (biome == BiomeKind.Swampland)
+								bitmap.SetPixel(x + xOffset, z + zOffset, Color.DarkOliveGreen);
+							else if (biome == BiomeKind.Jungle)
+								bitmap.SetPixel(x + xOffset, z + zOffset, Color.GreenYellow);
+							else if (biome == BiomeKind.JungleHills)
+								bitmap.SetPixel(x + xOffset, z + zOffset, Color.GreenYellow);
+							else if (biome == BiomeKind.Desert)
+								bitmap.SetPixel(x + xOffset, z + zOffset, Color.Yellow);
+							else if (biome == BiomeKind.DesertHills)
+								bitmap.SetPixel(x + xOffset, z + zOffset, Color.Yellow);
+							else if (biome == BiomeKind.Taiga)
+								bitmap.SetPixel(x + xOffset, z + zOffset, Color.White);
+							else if (biome == BiomeKind.TaigaHills)
+								bitmap.SetPixel(x + xOffset, z + zOffset, Color.WhiteSmoke);
+							else if (biome == BiomeKind.IcePlains)
+								bitmap.SetPixel(x + xOffset, z + zOffset, Color.White);
+							else if (biome == BiomeKind.IceMountains)
+								bitmap.SetPixel(x + xOffset, z + zOffset, Color.White);
+							else if (biome == BiomeKind.FrozenRiver)
+								bitmap.SetPixel(x + xOffset, z + zOffset, Color.AntiqueWhite);
+							else if (biome == BiomeKind.FrozenOcean)
+								bitmap.SetPixel(x + xOffset, z + zOffset, Color.CornflowerBlue);
 							else
 								bitmap.SetPixel(x + xOffset, z + zOffset, Color.Red);
 						}
