46cd933dd909cfb8d10afd1a8b15971fc66f25a8
Todd White <twhite@logos.com>
2013-10-19 13:04:03 -0700

    Some code that looks like hillshade algorithm.

--- src/MCSharp.ConsoleApp/Program.cs
+++ src/MCSharp.ConsoleApp/Program.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace MCSharp.ConsoleApp
 {
@@ -22,10 +23,110 @@ namespace MCSharp.ConsoleApp
 				return;
 			}
 
-			CreateHillShadeBitmap(outputLocation, saveInfo);
+			NewCreateHillshadeBitmap(outputLocation, saveInfo).Wait();
+			// CreateHillshadeBitmap(outputLocation, saveInfo);
 		}
 
-		private static void CreateHillShadeBitmap(string outputLocation, GameSaveInfo saveInfo)
+		private static async Task NewCreateHillshadeBitmap(string outputLocation, GameSaveInfo saveInfo)
+		{
+			Stopwatch stopwatch = Stopwatch.StartNew();
+
+			WorldReader reader = new WorldReader(saveInfo.Location);
+
+			GameSaveBounds bounds = await reader.GetCurrentWorldBoundsAsync();
+			
+			Bitmap bitmap = new Bitmap(bounds.BlockWidth, bounds.BlockHeight);
+			using (LockedBitmapWriter bitmapWriter = new LockedBitmapWriter(bitmap))
+			{
+				// cache height for each block
+				Dictionary<int, double> heightCache = new Dictionary<int, double>();
+
+				for (int zChunk = bounds.MinZChunk; zChunk <= bounds.MaxZChunk; zChunk++)
+				{
+					for (int xChunk = bounds.MinXChunk; xChunk <= bounds.MaxXChunk; xChunk++)
+					{
+						ChunkData chunk = await reader.GetChunkForChunkCoordinate(xChunk, zChunk);
+						if (chunk == null || chunk.IsEmpty)
+							continue;
+
+						int chunkLeft = chunk.XPosition.Value * Constants.ChunkBlockWidth;
+						int chunkTop = chunk.ZPosition.Value * Constants.ChunkBlockWidth;
+
+						int xOffset = chunkLeft - bounds.MinXBlock;
+						int zOffset = chunkTop - bounds.MinZBlock;
+
+						for (int z = 0; z < Constants.ChunkBlockWidth; z++)
+						{
+							for (int x = 0; x < Constants.ChunkBlockWidth; x++)
+							{
+								int imageX = x + xOffset;
+								int imageY = z + zOffset;
+
+								BiomeKind biome = chunk.GetBiome(x, z);
+
+								// calculate hillshade value
+								double hillshade = 0;
+								if (imageX > 0 && imageX < bounds.BlockWidth - 1 && imageY > 0 && imageY < bounds.BlockHeight - 1)
+								{
+									double a = chunk.GetHeight(z - 1, x - 1);
+									double b = chunk.GetHeight(z - 1, x);
+									double c = chunk.GetHeight(z - 1, x + 1);
+									double d = chunk.GetHeight(z, x - 1);
+									double e = chunk.GetHeight(z, x);
+									double f = chunk.GetHeight(z, x + 1);
+									double g = chunk.GetHeight(z + 1, x - 1);
+									double h = chunk.GetHeight(z + 1, x);
+									double i = chunk.GetHeight(z + 1, x + 1);
+
+									// compute hillshade
+									const int cellsize = 4; // no idea what this is...
+									const double zFactor = 0.25; // no idea what this is...
+
+									double dzOverDx = ((c + 2 * f + i) - (a + 2 * d + g)) / 8 * cellsize;
+									double dzOverDy = ((g + 2 * h + i) - (a + 2 * b + c)) / 8 * cellsize;
+
+									double slopeRad = Math.Atan(zFactor * Math.Sqrt(Math.Pow(dzOverDx, 2) + Math.Pow(dzOverDy, 2)));
+									double aspectRad = 0;
+									if (dzOverDx != 0)
+									{
+										aspectRad = Math.Atan2(dzOverDy, -dzOverDx);
+										if (aspectRad < 0)
+											aspectRad = 2 * Math.PI + aspectRad;
+									}
+									else if (dzOverDx == 0)
+									{
+										if (dzOverDy > 0)
+											aspectRad = Math.PI / 2.0;
+										else if (dzOverDy < 0)
+											aspectRad = 2 * Math.PI - Math.PI / 2;
+										else
+											aspectRad = aspectRad;
+									}
+
+									hillshade = 255.0 * ((Math.Cos(c_zenithRadians) * Math.Cos(slopeRad)) + (Math.Sin(c_zenithRadians) * Math.Sin(slopeRad) * Math.Cos(c_azimuthRadians - aspectRad)));
+									if (hillshade < 0)
+										hillshade = 0;
+								}
+
+								hillshade = (int) hillshade;
+
+								Color color = GetColorForBiomeKind(biome);
+								Color overlay = Color.FromArgb(150, (int) hillshade, (int) hillshade, (int) hillshade);
+
+								if (hillshade == 180)
+									overlay = Color.FromArgb(100, (int) hillshade, (int) hillshade, (int) hillshade);
+
+								bitmapWriter.SetPixel(imageX, imageY, BlendWith(color, overlay));
+							}
+						}
+					}
+				}
+			}
+
+			Console.WriteLine("Total time: {0}", stopwatch.ElapsedMilliseconds);
+		}
+
+		private static void CreateHillshadeBitmap(string outputLocation, GameSaveInfo saveInfo)
 		{
 			Stopwatch stopwatchTotal = Stopwatch.StartNew();
 
