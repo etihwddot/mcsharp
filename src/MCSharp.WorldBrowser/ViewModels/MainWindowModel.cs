777e4f27e2dbf20c9ff5731cd01d868c2f768dbe
Todd White <twhite@logos.com>
2013-10-20 20:53:37 -0700

    Basic logging support.

--- src/MCSharp.WorldBrowser/ViewModels/MainWindowModel.cs
+++ src/MCSharp.WorldBrowser/ViewModels/MainWindowModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
@@ -19,9 +20,9 @@ namespace MCSharp.WorldBrowser.ViewModels
 	{
 		public MainWindowModel()
 		{
+			m_log = new StringBuilder();
 			m_availableSaves = GameSaveInfo.GetAvailableSaves().ToList().AsReadOnly();
 			SelectedSave = m_availableSaves.FirstOrDefault();
-			m_elapsed = TimeSpan.FromMilliseconds(0);
 		}
 
 		public ReadOnlyCollection<GameSaveInfo> AvailableSaves
@@ -59,23 +60,27 @@ namespace MCSharp.WorldBrowser.ViewModels
 			}
 		}
 
-		public static readonly string ElapsedProperty = ExpressionUtility.GetPropertyName((MainWindowModel x) => x.Elapsed);
-		public TimeSpan Elapsed
+		public static readonly string LogProperty = ExpressionUtility.GetPropertyName((MainWindowModel x) => x.Log);
+		public string Log
 		{
 			get
 			{
 				VerifyAccess();
-				return m_elapsed;
+				return m_log.ToString();
 			}
-			set
-			{
-				VerifyAccess();
-				if (value != m_elapsed)
-				{
-					m_elapsed = value;
-					RaisePropertyChanged(ElapsedProperty);
-				}
-			}
+		}
+
+		private void WriteLine(string text)
+		{
+			m_log.AppendLine(text);
+			RaisePropertyChanged(LogProperty);
+		}
+
+		private void WriteLine(string format, params object[] args)
+		{
+			m_log.AppendFormat(format, args);
+			m_log.AppendLine();
+			RaisePropertyChanged(LogProperty);
 		}
 
 		private void GenerateImage()
@@ -97,9 +102,13 @@ namespace MCSharp.WorldBrowser.ViewModels
 
 		private async Task DoGenerateImage(CancellationToken token)
 		{
+			// Time the generate process
+			Stopwatch stopwatch = Stopwatch.StartNew();
+
+			// load the save
+			WriteLine("Loading: {0}", m_selectedSave.Name);
 			WorldSave save = await WorldSave.LoadAsync(m_selectedSave);
 
-			Stopwatch stopwatch = Stopwatch.StartNew();
 			TransformBlock<RegionInfo, Tuple<WorldSave, RegionInfo, byte[]>> regionToBytesTransform = new TransformBlock<RegionInfo, Tuple<WorldSave, RegionInfo, byte[]>>(x =>
 			{
 				byte[] bytes = GetRegionBytes(x);
@@ -126,7 +135,7 @@ namespace MCSharp.WorldBrowser.ViewModels
 			{
 			}
 
-			Elapsed = stopwatch.Elapsed;
+			WriteLine("Total time: {0}", stopwatch.Elapsed);
 		}
 
 		private void RenderRegion(WorldSave save, RegionInfo region, byte[] bytes)
@@ -194,11 +203,12 @@ namespace MCSharp.WorldBrowser.ViewModels
 
 		const int c_imageDpi = 96;
 
+		readonly StringBuilder m_log;
+
 		CancellationTokenSource m_source;
 		GameSaveInfo m_selectedSave;
 		ReadOnlyCollection<GameSaveInfo> m_availableSaves;
 		WriteableBitmap m_image;
-		TimeSpan m_elapsed;
-		Task m_imageTask;
+		Task m_imageTask;		
 	}
 }
