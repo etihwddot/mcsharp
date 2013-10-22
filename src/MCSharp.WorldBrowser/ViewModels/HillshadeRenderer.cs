f39ab8d502cbe788cc6351e28b78d1bcd33f03d8
Todd White <twhite@logos.com>
2013-10-21 21:19:22 -0700

    Easy way to no longer block UI thread in HillshadRenderer.

--- src/MCSharp.WorldBrowser/ViewModels/HillshadeRenderer.cs
+++ src/MCSharp.WorldBrowser/ViewModels/HillshadeRenderer.cs
@@ -36,108 +36,114 @@ namespace MCSharp.WorldBrowser.ViewModels
 
 			int[,] heightMap = new int[LengthUtility.RegionsToBlocks(save.Bounds.Height), LengthUtility.RegionsToBlocks(save.Bounds.Width)];
 
-			save.Regions.AsParallel().ForAll(region =>
+			await Task.Run(() =>
 			{
-				foreach (Chunk chunk in ChunkLoader.LoadChunksInRegion(region).Where(x => !x.IsEmpty))
+				save.Regions.AsParallel().ForAll(region =>
 				{
-					int xOffset = LengthUtility.ChunksToBlocks(chunk.XPosition.Value) - LengthUtility.RegionsToBlocks(save.Bounds.X);
-					int zOffset = LengthUtility.ChunksToBlocks(chunk.ZPosition.Value) - LengthUtility.RegionsToBlocks(save.Bounds.Z);
+					foreach (Chunk chunk in ChunkLoader.LoadChunksInRegion(region).Where(x => !x.IsEmpty))
+					{
+						int xOffset = LengthUtility.ChunksToBlocks(chunk.XPosition.Value) - LengthUtility.RegionsToBlocks(save.Bounds.X);
+						int zOffset = LengthUtility.ChunksToBlocks(chunk.ZPosition.Value) - LengthUtility.RegionsToBlocks(save.Bounds.Z);
 
-					int chunkBlockWidth = LengthUtility.ChunksToBlocks(1);
+						int chunkBlockWidth = LengthUtility.ChunksToBlocks(1);
 
-					for (int z = 0; z < chunkBlockWidth; z++)
-						for (int x = 0; x < chunkBlockWidth; x++)
-							heightMap[z + zOffset, x + xOffset] = chunk.GetHeight(x, z);
-				}
+						for (int z = 0; z < chunkBlockWidth; z++)
+							for (int x = 0; x < chunkBlockWidth; x++)
+								heightMap[z + zOffset, x + xOffset] = chunk.GetHeight(x, z);
+					}
+				});
 			});
 
-			save.Regions.AsParallel().ForAll(region =>
+			await Task.Run(() =>
 			{
-				IEnumerable<Chunk> regionChunks = ChunkLoader.LoadChunksInRegion(region);
-
-				int regionBlockWidth = LengthUtility.RegionsToBlocks(1);
-
-				foreach (Chunk chunk in regionChunks.Where(x => !x.IsEmpty))
+				save.Regions.AsParallel().ForAll(region =>
 				{
-					int xOffset = LengthUtility.ChunksToBlocks(chunk.XPosition.Value) - LengthUtility.RegionsToBlocks(save.Bounds.X);
-					int zOffset = LengthUtility.ChunksToBlocks(chunk.ZPosition.Value) - LengthUtility.RegionsToBlocks(save.Bounds.Z);
+					IEnumerable<Chunk> regionChunks = ChunkLoader.LoadChunksInRegion(region);
 
-					int chunkBlockWidth = LengthUtility.ChunksToBlocks(1);
+					int regionBlockWidth = LengthUtility.RegionsToBlocks(1);
 
-					for (int z = 0; z < chunkBlockWidth; z++)
+					foreach (Chunk chunk in regionChunks.Where(x => !x.IsEmpty))
 					{
-						for (int x = 0; x < chunkBlockWidth; x++)
+						int xOffset = LengthUtility.ChunksToBlocks(chunk.XPosition.Value) - LengthUtility.RegionsToBlocks(save.Bounds.X);
+						int zOffset = LengthUtility.ChunksToBlocks(chunk.ZPosition.Value) - LengthUtility.RegionsToBlocks(save.Bounds.Z);
+
+						int chunkBlockWidth = LengthUtility.ChunksToBlocks(1);
+
+						for (int z = 0; z < chunkBlockWidth; z++)
 						{
-							int imageX = x + xOffset;
-							int imageY = z + zOffset;
-
-							BiomeKind biome = chunk.GetBiome(x, z);
-
-							double hillshade = 0;
-							if (imageX > 0 && imageX < LengthUtility.RegionsToBlocks(save.Bounds.Width) - 1 &&
-								imageY > 0 && imageY < LengthUtility.RegionsToBlocks(save.Bounds.Height) - 1)
+							for (int x = 0; x < chunkBlockWidth; x++)
 							{
-								double a = heightMap[imageY - 1, imageX - 1];
-								double b = heightMap[imageY - 1, imageX];
-								double c = heightMap[imageY - 1, imageX + 1];
-								double d = heightMap[imageY, imageX - 1];
-								double e = heightMap[imageY, imageX];
-								double f = heightMap[imageY, imageX + 1];
-								double g = heightMap[imageY + 1, imageX - 1];
-								double h = heightMap[imageY + 1, imageX];
-								double i = heightMap[imageY + 1, imageX + 1];
+								int imageX = x + xOffset;
+								int imageY = z + zOffset;
 
-								// compute hillshade
-								const int cellsize = 4; // no idea what this is...
-								const double zFactor = 0.25; // no idea what this is...
+								BiomeKind biome = chunk.GetBiome(x, z);
 
-								double dzOverDx = ((c + 2 * f + i) - (a + 2 * d + g)) / 8 * cellsize;
-								double dzOverDy = ((g + 2 * h + i) - (a + 2 * b + c)) / 8 * cellsize;
-
-								double slopeRad = Math.Atan(zFactor * Math.Sqrt(Math.Pow(dzOverDx, 2) + Math.Pow(dzOverDy, 2)));
-								double aspectRad = 0;
-								if (dzOverDx != 0)
+								double hillshade = 0;
+								if (imageX > 0 && imageX < LengthUtility.RegionsToBlocks(save.Bounds.Width) - 1 &&
+									imageY > 0 && imageY < LengthUtility.RegionsToBlocks(save.Bounds.Height) - 1)
 								{
-									aspectRad = Math.Atan2(dzOverDy, -dzOverDx);
-									if (aspectRad < 0)
-										aspectRad = 2 * Math.PI + aspectRad;
-								}
-								else if (dzOverDx == 0)
-								{
-									if (dzOverDy > 0)
-										aspectRad = Math.PI / 2.0;
-									else if (dzOverDy < 0)
-										aspectRad = 2 * Math.PI - Math.PI / 2;
-									else
-										aspectRad = aspectRad;
+									double a = heightMap[imageY - 1, imageX - 1];
+									double b = heightMap[imageY - 1, imageX];
+									double c = heightMap[imageY - 1, imageX + 1];
+									double d = heightMap[imageY, imageX - 1];
+									double e = heightMap[imageY, imageX];
+									double f = heightMap[imageY, imageX + 1];
+									double g = heightMap[imageY + 1, imageX - 1];
+									double h = heightMap[imageY + 1, imageX];
+									double i = heightMap[imageY + 1, imageX + 1];
+
+									// compute hillshade
+									const int cellsize = 4; // no idea what this is...
+									const double zFactor = 0.25; // no idea what this is...
+
+									double dzOverDx = ((c + 2 * f + i) - (a + 2 * d + g)) / 8 * cellsize;
+									double dzOverDy = ((g + 2 * h + i) - (a + 2 * b + c)) / 8 * cellsize;
+
+									double slopeRad = Math.Atan(zFactor * Math.Sqrt(Math.Pow(dzOverDx, 2) + Math.Pow(dzOverDy, 2)));
+									double aspectRad = 0;
+									if (dzOverDx != 0)
+									{
+										aspectRad = Math.Atan2(dzOverDy, -dzOverDx);
+										if (aspectRad < 0)
+											aspectRad = 2 * Math.PI + aspectRad;
+									}
+									else if (dzOverDx == 0)
+									{
+										if (dzOverDy > 0)
+											aspectRad = Math.PI / 2.0;
+										else if (dzOverDy < 0)
+											aspectRad = 2 * Math.PI - Math.PI / 2;
+										else
+											aspectRad = aspectRad;
+									}
+
+									hillshade = 255.0 * ((Math.Cos(c_zenithRadians) * Math.Cos(slopeRad)) + (Math.Sin(c_zenithRadians) * Math.Sin(slopeRad) * Math.Cos(c_azimuthRadians - aspectRad)));
+									if (hillshade < 0)
+										hillshade = 0;
 								}
 
-								hillshade = 255.0 * ((Math.Cos(c_zenithRadians) * Math.Cos(slopeRad)) + (Math.Sin(c_zenithRadians) * Math.Sin(slopeRad) * Math.Cos(c_azimuthRadians - aspectRad)));
-								if (hillshade < 0)
-									hillshade = 0;
+								hillshade = (int) hillshade;
+
+								byte hillshadeByte = (byte) hillshade;
+
+								ColorBgra32 color = BiomeKindUtility.GetColor(biome);
+								ColorBgra32 overlay = ColorBgra32.FromArgb(150, hillshadeByte, hillshadeByte, hillshadeByte);
+
+								if (hillshade == 180)
+									overlay = ColorBgra32.FromArgb(100, hillshadeByte, hillshadeByte, hillshadeByte);
+
+								target.SetPixel(imageX, imageY, ColorBgra32.Blend(color, overlay));
 							}
+						}
 
-							hillshade = (int) hillshade;
 
-							byte hillshadeByte = (byte) hillshade;
-
-							ColorBgra32 color = BiomeKindUtility.GetColor(biome);
-							ColorBgra32 overlay = ColorBgra32.FromArgb(150, hillshadeByte, hillshadeByte, hillshadeByte);
-
-							if (hillshade == 180)
-								overlay = ColorBgra32.FromArgb(100, hillshadeByte, hillshadeByte, hillshadeByte);
-
-							target.SetPixel(imageX, imageY, ColorBgra32.Blend(color, overlay));
+						if (chunk.XPosition == 0 && chunk.ZPosition == 0)
+						{
+							originXOffset = xOffset;
+							originZOffset = zOffset;
 						}
 					}
-
-
-					if (chunk.XPosition == 0 && chunk.ZPosition == 0)
-					{
-						originXOffset = xOffset;
-						originZOffset = zOffset;
-					}
-				}
+				});
 			});
 		}
 
