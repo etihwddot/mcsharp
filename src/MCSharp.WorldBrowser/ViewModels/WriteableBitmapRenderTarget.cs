ea7fc2d7dd1f8b5880ba2ed750b2ac508de45d19
Todd White <twhite@logos.com>
2013-10-21 21:36:00 -0700

    Adding naive approach to writing pixels.

--- src/MCSharp.WorldBrowser/ViewModels/WriteableBitmapRenderTarget.cs
+++ src/MCSharp.WorldBrowser/ViewModels/WriteableBitmapRenderTarget.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows;
 using System.Windows.Media.Imaging;
 using MCSharp.Utility;
@@ -14,16 +15,39 @@ namespace MCSharp.WorldBrowser.ViewModels
 				throw new ArgumentNullException("image");
 
 			m_image = image;
-			m_image.Lock();
-			m_buffer = m_image.BackBuffer;
 			m_width = m_image.PixelWidth;
 			m_bytesPerPixel = m_image.Format.BitsPerPixel / 8;
+			m_lock = new object();
+
+			m_image.Lock();
+			m_buffer = m_image.BackBuffer;
 		}
 
 		public void SetPixel(int x, int y, ColorBgra32 color)
 		{
+			lock (m_lock)
+			{
+				if (m_pixelWrites > 4096)
+				{
+					m_image.Dispatcher.Invoke(() =>
+					{
+						m_image.AddDirtyRect(new Int32Rect(0, 0, m_image.PixelWidth, m_image.PixelHeight));
+						m_image.Unlock();
+					});
+
+					m_image.Dispatcher.Invoke(() =>
+					{
+						m_image.Lock();
+						m_buffer = m_image.BackBuffer;
+						m_pixelWrites = 0;
+					});
+				}
+			}
+
+			// write the pixel
 			IntPtr currentPixel = new IntPtr(m_buffer.ToInt64() + y * (m_width * m_bytesPerPixel) + (x * m_bytesPerPixel));
 			Marshal.Copy(new byte[] { color.Blue, color.Green, color.Red, color.Alpha }, 0, currentPixel, m_bytesPerPixel);
+			Interlocked.Increment(ref m_pixelWrites);
 		}
 
 		public void Dispose()
@@ -33,8 +57,11 @@ namespace MCSharp.WorldBrowser.ViewModels
 		}
 
 		readonly WriteableBitmap m_image;
-		readonly IntPtr m_buffer;
 		readonly int m_width;
 		readonly int m_bytesPerPixel;
+
+		IntPtr m_buffer;
+		object m_lock;
+		int m_pixelWrites;
 	}
 }
